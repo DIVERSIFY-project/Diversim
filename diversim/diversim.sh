47c1774193f1a12d1863471a405be86f71e7f34c
Marco Biazzini <Marco.Biazzini@inria.fr>
2013-09-04 16:06:50 +0200

    add scripts, jars, pom and docs

---
+++ diversim/diversim.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+binDir=$HOME/workspace/diversim/bin # where are the binaries
+libDir=$HOME/Diversify/mason # where are the libs
+
+libs=$libDir/jmf.jar:$libDir/itext-1.2.jar:$libDir/portfolio.jar:$libDir/jcommon-1.0.16.jar:$libDir/jfreechart-1.0.13.jar:$libDir/jar/mason.17.jar
+
+diversim_cmdLine="java -Xmx256M -cp $binDir:$libs diversim.BipartiteGraph" # WithUI
+diversim_params="-for 2000" # 2>/dev/null -seed 1 -time 1000 -for 200000
+
+if [ $# -lt 1 ] ; then
+  eval $diversim_cmdLine $diversim_params
+else
+  eval $diversim_cmdLine $@
+fi
