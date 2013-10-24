d99cd34d6ac1e7d92ce27f8567c41f1dba02a154
Andre Elie <andre.elie@inria.fr>
2013-10-24 13:38:14 +0200

    added a managePopulation methods which aims at maintening a population size constant minor corrections & evolutions

--- diversim/src/main/java/diversim/strategy/fate/AndreFateOfDoom.java
+++ diversim/src/main/java/diversim/strategy/fate/AndreFateOfDoom.java
@@ -1,12 +1,15 @@
 package diversim.strategy.fate;
 
 
+import java.util.Comparator;
+import java.util.List;
+
+import sim.util.Bag;
 import diversim.model.App;
 import diversim.model.BipartiteGraph;
 import diversim.model.Fate;
-import diversim.model.Service;
+import diversim.model.Platform;
 import diversim.strategy.AbstractStrategy;
-import diversim.strategy.application.LinkStrategy;
 
 
 /**
@@ -19,25 +22,57 @@ public AndreFateOfDoom() {
 }
 
 
+@SuppressWarnings("unchecked")
 @Override
 public void evolve(BipartiteGraph graph, Fate agent) {
-	// kill
-	// 25% Backdoor / 25% Obsolescence / 25% UnattendedService / 25% Concentration
-	/*
-	 * Service backdoor = graph.services.get((int)(Math.random() * graph.services.size())); switch
-	 * ((int)(Math.random() * 0)) { case 0: KillFates.backdoor(graph, backdoor, 0.01); break; case 1:
-	 * KillFates.obsolescence(graph, 0.01); break; case 2: KillFates.unattended(graph, 0.01); break;
-	 * default: KillFates.concentration(graph); }
-	 */
-	KillFates.unattended(graph, 0.01);
-	// creation
-	CreationFates.cloningRandom(graph, 0.01);
+	// managePopulation(graph);
+	Bag platforms = new Bag(graph.platforms);
+	platforms.sort(new Comparator() {
+
+		@Override
+		public int compare(Object o1, Object o2) {
+			return ((Platform)o1).getSize() - ((Platform)o2).getSize();
+		}
+
+	});
+	KillFates.gasFactoryExact(graph, (int)(graph.getNumServices() * 0.9), 1);
+	CreationFates.splitExact(graph, 1);
 	// mutation
 	MutationFates.bugCorrected(graph);
 	// linking
-	// LinkStrategyFates.ExclusiveServicesLinkStrategy(graph);
+	LinkStrategyFates.linkingB(graph);
 	for (App app : graph.apps) {
 		app.step(graph);
 	}
+	for (Platform platform : graph.platforms) {
+		platform.step(graph);
+	}
+}
+
+
+public void managePopulation(BipartiteGraph graph) {
+	double killCreateStep = 0.1;
+	double killThreshold = 0.3;
+	double createThreshold = 0.3;
+	double growthStep = 0.1;
+	double growthMargin = 0.5;
+	if (graph.getNumPlatforms() <= (1 + growthMargin) * graph.getInitPlatforms()
+	    && graph.getNumPlatforms() >= (1 - growthMargin) * graph.getInitPlatforms()) {
+		killThreshold = 0.3;
+		createThreshold = 0.3;
+	}
+	if (graph.getNumPlatforms() > (1 + growthMargin) * graph.getInitPlatforms()) {
+		killThreshold -= growthStep;
+		createThreshold += growthStep;
+	}
+	if (graph.getNumPlatforms() < (1 - growthMargin) * graph.getInitPlatforms()) {
+		killThreshold += growthStep;
+		createThreshold -= growthStep;
+	}
+	// killing
+	if (graph.random().nextDouble() > killThreshold) KillFates.random(graph, killCreateStep);
+	// creation
+	if (graph.random().nextDouble() > createThreshold)
+	  CreationFates.cloningRandom(graph, killCreateStep);
 }
 }
