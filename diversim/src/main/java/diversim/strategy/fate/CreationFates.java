af23e4e9a4880208a1185b535d854599bd1ce32e
Andre Elie <andre.elie@inria.fr>
2013-10-31 17:58:56 +0100

    deep rewrite of creation methods

--- diversim/src/main/java/diversim/strategy/fate/CreationFates.java
+++ diversim/src/main/java/diversim/strategy/fate/CreationFates.java
@@ -6,6 +6,7 @@ import java.util.List;
 import java.util.logging.Level;
 import java.util.logging.Logger;
 
+import sim.util.Bag;
 import diversim.model.BipartiteGraph;
 import diversim.model.Platform;
 import diversim.model.Service;
@@ -16,87 +17,93 @@ public class CreationFates {
 public CreationFates() {}
 
 
-public static void cloningRandom(BipartiteGraph graph, double populationSize) {
-	for (int i = 0; i < graph.getNumPlatforms() * populationSize; i++) {
-		Platform source = graph.platforms.get(graph.random().nextInt(graph.getNumPlatforms()));
-		Platform clone = graph.createPlatform(source.getKind());
-		for (Service s : source.getServices()) {
-			BipartiteGraph.addUnique(clone.getServices(), s);
-		}
-		clone.setStrategy(source.getStrategy());
-		Logger.getLogger(CreationFates.class.getName()).log(Level.INFO,
-		    "Platform <" + source + "> has been cloned");
-	}
-}
-
-
-public static void cloningMutateRandom(BipartiteGraph graph, double populationSize,
+public static void cloningMutateRandom(BipartiteGraph graph, double populationCreationRatio,
     double servicesSizeVariation) {
-	for (int i = 0; i < graph.getNumPlatforms() * populationSize; i++) {
-		Platform source = graph.platforms.get(graph.random().nextInt(graph.getNumPlatforms()));
-		Platform clone = graph.createPlatform(source.getKind());
-		int cloneSize = Math.min(graph.random()
-		    .nextInt((int)(source.getSize() * servicesSizeVariation)), graph.getNumServices());
-		List<Service> cloneServices = new ArrayList<Service>();
-		for (int j = 0; j < cloneSize; j++) {
-			cloneServices.add(graph.services.get(graph.random().nextInt(graph.getNumServices())));
+	if (graph.getNumPlatforms() * (1 + populationCreationRatio) <= graph.getMaxPlatforms()) {
+		for (int i = 0; i < graph.getNumPlatforms() * populationCreationRatio; i++) {
+			Platform source = graph.platforms.get(graph.random().nextInt(graph.getNumPlatforms()));
+			Platform clone = graph.createPlatform(source.getKind());
+			clone.getServices().clear();
+			int cloneSize = Math.min(
+			    graph.random().nextInt((int)(source.getSize() * servicesSizeVariation)),
+			    graph.getNumServices());
+			List<Service> cloneServices = new ArrayList<Service>();
+			for (int j = 0; j < cloneSize; j++) {
+				cloneServices.add(graph.services.get(graph.random().nextInt(graph.getNumServices())));
+			}
+			for (Service s : cloneServices) {
+				BipartiteGraph.addUnique(clone.getServices(), s);
+			}
+			clone.setStrategy(source.getStrategy());
+			Logger.getLogger(CreationFates.class.getName()).log(Level.INFO,
+			    "Platform <" + source + "> has been cloned");
 		}
-		for (Service s : cloneServices) {
-			BipartiteGraph.addUnique(clone.getServices(), s);
-		}
-		clone.setStrategy(source.getStrategy());
-		Logger.getLogger(CreationFates.class.getName()).log(Level.INFO,
-		    "Platform <" + source + "> has been cloned");
+	} else {
+		Logger.getLogger(CreationFates.class.getName()).log(Level.FINEST,
+		    "CloningMutate: max number of Platforms reached");
 	}
 }
 
 
-public static void split(BipartiteGraph graph, double populationSize) {
-	for (int i = 0; i < graph.getNumPlatforms() * populationSize; i++) {
-		Platform source = graph.platforms.get(graph.random().nextInt(graph.getNumPlatforms()));
-		Platform clone1 = graph.createPlatform(source.getKind());
-		Platform clone2 = graph.createPlatform(source.getKind());
-		List<Service> clone1Services = source.getServices().subList(0, source.getServices().size() / 2);
-		for (Service s : clone1Services) {
-			BipartiteGraph.addUnique(clone1.getServices(), s);
+public static void split(BipartiteGraph graph, double servicesTransmitted, double populationCreationRatio) {
+	if (graph.getNumPlatforms() * (1 + populationCreationRatio) < graph.getMaxPlatforms()) {
+		for (int i = 0; i < graph.getNumPlatforms() * populationCreationRatio; i++) {
+			Platform source = graph.platforms.get(graph.random().nextInt(graph.getNumPlatforms()));
+			Platform clone1 = graph.createPlatform(source.getKind());
+			clone1.getServices().clear();
+			Platform clone2 = graph.createPlatform(source.getKind());
+			clone2.getServices().clear();
+			Bag sourceServices = new Bag(source.getServices());
+			sourceServices.shuffle(graph.random());
+			for (int j = 0; j < sourceServices.size() * servicesTransmitted; j++) {
+				BipartiteGraph.addUnique(clone1.getServices(), (Service)sourceServices.get(j));
+			}
+			sourceServices.shuffle(graph.random());
+			for (int j = 0; j < sourceServices.size() * servicesTransmitted; j++) {
+				BipartiteGraph.addUnique(clone2.getServices(), (Service)sourceServices.get(j));
+			}
+			clone1.setStrategy(source.getStrategy());
+			clone2.setStrategy(source.getStrategy());
+			graph.removeEntity(graph.platforms, source);
+			Logger.getLogger(CreationFates.class.getName()).log(
+			    Level.INFO,
+			    "Platform <" + source + "> has been split into platforms <" + clone1 + "> & <" + clone2
+			        + ">");
 		}
-		clone1.setStrategy(source.getStrategy());
-		List<Service> clone2Services = source.getServices().subList(source.getServices().size() / 2,
-		    source.getServices().size());
-		for (Service s : clone2Services) {
-			BipartiteGraph.addUnique(clone2.getServices(), s);
-		}
-		clone2.setStrategy(source.getStrategy());
-		graph.removeEntity(graph.platforms, source);
-		Logger.getLogger(CreationFates.class.getName()).log(
-		    Level.INFO,
-		    "Platform <" + source + "> has been split into platforms <" + clone1 + "> & <" + clone2
-		        + ">");
+	} else {
+		Logger.getLogger(CreationFates.class.getName()).log(Level.FINEST,
+		    "Split: max number of Platforms reached");
 	}
 }
 
 
-public static void splitExact(BipartiteGraph graph, int amount) {
-	for (int i = 0; i < amount; i++) {
-		Platform source = graph.platforms.get(graph.random().nextInt(graph.getNumPlatforms()));
-		Platform clone1 = graph.createPlatform(source.getKind());
-		Platform clone2 = graph.createPlatform(source.getKind());
-		List<Service> clone1Services = source.getServices().subList(0, source.getServices().size() / 2);
-		for (Service s : clone1Services) {
-			BipartiteGraph.addUnique(clone1.getServices(), s);
+public static void splitExact(BipartiteGraph graph, double servicesTransmitted, int amount) {
+	if (graph.getNumPlatforms() < graph.getMaxPlatforms()) {
+		for (int i = 0; i < amount; i++) {
+			Platform source = graph.platforms.get(graph.random().nextInt(graph.getNumPlatforms()));
+			Platform clone1 = graph.createPlatform(source.getKind());
+			clone1.getServices().clear();
+			Platform clone2 = graph.createPlatform(source.getKind());
+			clone2.getServices().clear();
+			Bag sourceServices = new Bag(source.getServices());
+			sourceServices.shuffle(graph.random());
+			for (int j = 0; j < sourceServices.size() * servicesTransmitted; j++) {
+				BipartiteGraph.addUnique(clone1.getServices(), (Service)sourceServices.get(j));
+			}
+			sourceServices.shuffle(graph.random());
+			for (int j = 0; j < sourceServices.size() * servicesTransmitted; j++) {
+				BipartiteGraph.addUnique(clone2.getServices(), (Service)sourceServices.get(j));
+			}
+			clone2.setStrategy(source.getStrategy());
+			graph.removeEntity(graph.platforms, source);
+			Logger.getLogger(CreationFates.class.getName()).log(
+			    Level.INFO,
+			    "Platform <" + source + "> has been split into platforms <" + clone1 + "> & <" + clone2
+			        + ">");
 		}
-		clone1.setStrategy(source.getStrategy());
-		List<Service> clone2Services = source.getServices().subList(source.getServices().size() / 2,
-		    source.getServices().size());
-		for (Service s : clone2Services) {
-			BipartiteGraph.addUnique(clone2.getServices(), s);
-		}
-		clone2.setStrategy(source.getStrategy());
-		graph.removeEntity(graph.platforms, source);
-		Logger.getLogger(CreationFates.class.getName()).log(
-		    Level.INFO,
-		    "Platform <" + source + "> has been split into platforms <" + clone1 + "> & <" + clone2
-		        + ">");
+	} else {
+		Logger.getLogger(CreationFates.class.getName()).log(Level.FINEST,
+		    "SplitExact: max number of Platforms reached");
 	}
 }
 }
