ffb0bb31c06eb4a4ff22c4a56151fef8f6e071f8
Andre Elie <andre.elie@inria.fr>
2013-12-12 14:48:33 +0100

    using linking strategy C

--- diversim/src/main/java/diversim/strategy/fate/AndreFateOfDoom.java
+++ diversim/src/main/java/diversim/strategy/fate/AndreFateOfDoom.java
@@ -37,7 +37,7 @@ public void evolve(BipartiteGraph graph, Fate agent) {
 	//
 	MutationFates.random(graph, 0.1, 0.1);
 	// linking
-	LinkStrategyFates.linkingA(graph);
+	LinkStrategyFates.linkingC(graph);
 	for (App app : graph.apps) {
 		app.step(graph);
 	}
