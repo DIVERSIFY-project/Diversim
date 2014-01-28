bf9ed8c208f0ca545940337d31fdf1a1394bfbf0
Andre Elie <andre.elie@inria.fr>
2014-01-28 15:07:34 +0100

    added increaseOffer method

--- diversim/src/main/java/diversim/strategy/fate/MutationFates.java
+++ diversim/src/main/java/diversim/strategy/fate/MutationFates.java
@@ -7,14 +7,48 @@ import java.util.List;
 
 import sim.util.Bag;
 import diversim.model.BipartiteGraph;
+import diversim.model.Entity;
 import diversim.model.Platform;
 import diversim.model.Service;
 import diversim.util.Log;
+import ec.util.MersenneTwisterFast;
 
 
 public class MutationFates {
 
 public MutationFates() {}
+
+
+@SuppressWarnings("rawtypes")
+public static <T extends Comparable> List<T> getRandomEntities(List<T> source, int amount,
+    MersenneTwisterFast random) {
+	List<T> results = new ArrayList<T>();
+	if (amount >= source.size()) {
+		Log.warn("Requested amount too big in getRandomEntities, returning entire source");
+		return source;
+	}
+	for (int i = 0; i < amount; i++) {
+		BipartiteGraph.addUnique(results, source.get(random.nextInt(source.size())));
+	}
+	return results;
+}
+
+
+public static void increaseOffer(BipartiteGraph graph, double addedServicesRatio,
+    double mutatedPlatformsRatio) {
+	int addedServicesNumber = (int)Math.ceil(graph.getNumServices() * addedServicesRatio);
+	int mutatedPopulationNumber = (int)Math.ceil(graph.getNumPlatforms() * mutatedPlatformsRatio);
+	List<Platform> mutatedPlatforms = getRandomEntities(graph.platforms, mutatedPopulationNumber,
+	    graph.random());
+	int targetSize;
+	for (Platform platform : mutatedPlatforms) {
+		targetSize = Math.min(platform.getSize() + addedServicesNumber, graph.services.size());
+		while (platform.getSize() < targetSize) {
+			Service candidateService = getRandomEntities(graph.services, 1, graph.random()).get(0);
+			BipartiteGraph.addUnique(platform.getServices(), candidateService);
+		}
+	}
+}
 
 
 public static void bugCorrected(BipartiteGraph graph) {
@@ -54,12 +88,12 @@ public static void upgrade(BipartiteGraph graph, int upgradedServicesNumber) {
 }
 
 
-public static void random(BipartiteGraph graph, double populationSize, double mutationSize) {
-	int counterPopulation = Math.max((int)(graph.getNumPlatforms() * populationSize), 1);
+public static void random(BipartiteGraph graph, double mutatedPopulationRatio, double mutationRatio) {
+	int counterPopulation = Math.max((int)(graph.getNumPlatforms() * mutatedPopulationRatio), 1);
 	Bag platforms = new Bag(graph.platforms);
 	platforms.shuffle(graph.random());
 	for (Object platform : platforms) {
-		int counterMutation = Math.max((int)(((Platform)platform).getSize() * mutationSize),
+		int counterMutation = Math.max((int)(((Platform)platform).getSize() * mutationRatio),
 		    1);
 		int removedServices = counterMutation;
 		Bag services = new Bag(((Platform)platform).getServices());
