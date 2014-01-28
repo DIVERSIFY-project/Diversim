ecd09a630d8de2278eab1e0288b18ad01ad67d4d
Andre Elie <andre.elie@inria.fr>
2014-01-28 15:06:09 +0100

    minor format changes

--- diversim/src/main/java/diversim/strategy/Strategy.java
+++ diversim/src/main/java/diversim/strategy/Strategy.java
@@ -3,18 +3,19 @@ package diversim.strategy;
 import diversim.model.BipartiteGraph;
 import sim.engine.Steppable;
 
+
 /**
- * User: Simon
- * Date: 7/8/13
- * Time: 10:11 AM
+ * User: Simon Date: 7/8/13 Time: 10:11 AM
  */
 public interface Strategy<T extends Steppable> {
 
-    /**
-     * @param agent the model which is evolve
-     * @param graph The bipartitegraph.
-     */
-    public void evolve(BipartiteGraph graph, T agent);
+/**
+ * @param agent
+ *          the model which is evolve
+ * @param graph
+ *          The bipartitegraph.
+ */
+public void evolve(BipartiteGraph graph, T agent);
 
 void init(String stratId);
 
