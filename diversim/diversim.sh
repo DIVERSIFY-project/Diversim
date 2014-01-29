9ddfddd943817c9bf78ee12dcc31424e426accec
Marco Biazzini <Marco.Biazzini@inria.fr>
2014-01-29 13:44:03 +0100

    add test config and update launch script

--- diversim/diversim.sh
+++ diversim/diversim.sh
@@ -9,10 +9,11 @@ steps=1000000
 time=1000
 
 #diversim_cmdLine="java -Xmx256M -cp $libs:$binDir diversim.model.Fate" # test distributions
-diversim_cmdLine="java -Xmx256M -cp $libs:$binDir diversim.BipartiteGraphWithUI" # GUI
-#diversim_cmdLine="java -Xmx256M -cp $libs:$binDir diversim.model.BipartiteGraph" # NO GUI
+#diversim_cmdLine="java -Xmx256M -cp $libs:$binDir diversim.BipartiteGraphWithUI" # GUI
+diversim_cmdLine="java -Xmx256M -cp $libs:$binDir diversim.model.BipartiteGraph" # NO GUI
 
-diversim_params= #"-for $steps -time $time" #-for 2000 # 2>/dev/null # -seed 1 -time 1000
+diversim_params="-configfolder $baseDir/testConf -resultfolder $baseDir/res -size 2 -step 500 -sample 5"
+#"-for $steps -time $time" #-for 2000 # 2>/dev/null # -seed 1 -time 1000
 
 if [ $# -lt 1 ] ; then
    eval $diversim_cmdLine $diversim_params
